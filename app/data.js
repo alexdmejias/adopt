fa57e912dd0bb021d757ec79defc307bf2bc899d
Alexis Desktop <amejias101@qc.cuny.edu>
2012-04-21 23:30:09 -0400

    init

---
+++ app/data.js
@@ -0,0 +1,25 @@
+App.Data = (function(lng, app, undefined) {
+
+    return {
+    	test : [
+    	        {
+            "id":3,
+            "date": "12/10/2011",
+            "type": "Walk",
+            "distance": "2.5 miles",
+            "comments": "Shouldn't have taken the dog",
+            "minutes": 45
+        },
+        {
+            "id":4,
+            "date": "12/09/2011",
+            "type": "Run",
+            "distance": "Long",
+            "comments": "Legs felt good",
+            "minutes": 75
+        }
+]
+
+    }
+
+})(LUNGO, App);
